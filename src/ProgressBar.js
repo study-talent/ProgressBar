6c4e5a9cdb15b220c2651f7e059bb1e28b10b1bb
study-talent <studytalentsn@gmail.com>
2021-09-18 22:52:56 +0530

    Initial commit

---
+++ src/ProgressBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function ProgressBar(props) {
+    const [counter,setCounter] = useState(10);
+    if(counter<=100) {
+      setTimeout(() => {
+      setCounter(counter+10)
+  }, 10000);
+  }
+    return (
+      <View style={{  height:50, backgroundColor: "blue", width:`${counter}%` }} />
+    );
+  }
+
+export default ProgressBar;
